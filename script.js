b067ae562d719682be7270355df71901ece03e1b
aharon <03manukyan03@gmail.com>
2021-09-06 11:32:04 +0300

    base tasks all complete

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ fetch("data.json")
   })
   .then(data => {
     for (let i = 0; i < data.length; i++) {
-      const {name,phone,about,eyeColor} = data[i];
+      const {id,name,phone,about,eyeColor} = data[i];
       const {firstName,lastName}  = name;
-      const item = {firstName, lastName, about, eyeColor, phone}
+      const item = {id,firstName, lastName, about, eyeColor, phone}
       dataArr.push(item);
     }
     addRowsToTable(dataArr)
@@ -36,16 +36,15 @@ const createElement = (elementName,className = null,value = null,parent = null) 
     el.className = className;
   }
   if(value !== null){
-    el.textContent = value; // для остальных
-    el.value = value //для input
+    el.textContent = value;
   }
   if(parent !== null){
     parent.appendChild(el);
   }
   return el;
 }
-const deleteTbody = () => {
-  tbody.textContent = "";
+const deleteTextContent = (element) => {
+  element.textContent = "";
 }
 
 const createTable = (obj) => {
@@ -60,12 +59,14 @@ const createTable = (obj) => {
 
   return tbl;
 }
-
 const addRowsToTable = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     let tr = createElement("tr","table-row", null,tbody );
     tr.addEventListener("click",() => editCell(i));
     for (let objKey in arr[i]) {
+      if(objKey === "id"){
+        continue;
+      }
       if(arr[i][objKey].length > 30){
         createElement("td","table-data",textTruncate(arr[i][objKey]),tr);
       }else{
@@ -82,50 +83,23 @@ const textTruncate = (str, length = 40, ending = "...") => {
     return str;
   }
 }
-
-const editCell = (j) => {
-  rightDiv.textContent = "";
-
-  const form = createElement("form","form-group",null,rightDiv);
-
-  createElement("h3",null,"Change Cell Information",form);
-
-  for (let key in dataArr[j]) {
-    if(dataArr[j][key].length > 30 ){
-        createElement("textarea","edit-textarea",dataArr[j][key],form);
-    }else{
-      let input = createElement("input","edit-input",dataArr[j][key],form);
-      input.type = "text";
-    }
-  }
-  const btn = createElement("button","btn","Change",form);
-
-  btn.addEventListener("click",() => changeInfoToCell())
-
-  rightDiv.appendChild(form);
-}
-
-const changeInfoToCell = () => {
-  console.log("id")
-}
-
 const sortingColumnAsc = (item) => {
-  deleteTbody();
+  deleteTextContent(tbody);
   for (let i = 0; i < dataArr.length; i+=1) {
-      for (let j = 0; j < dataArr.length-1-i; j+=1) {
-          if(dataArr[j][item] > dataArr[j+1][item]){
-            let tmp = dataArr[j];
-            dataArr[j] = dataArr[j+1];
-            dataArr[j+1] = tmp;
-          }
+    for (let j = 0; j < dataArr.length-1-i; j+=1) {
+      if(dataArr[j][item] > dataArr[j+1][item]){
+        let tmp = dataArr[j];
+        dataArr[j] = dataArr[j+1];
+        dataArr[j+1] = tmp;
       }
     }
+  }
 
   addRowsToTable(dataArr);
 
 }
 const sortingColumnDesc = (item) => {
-  deleteTbody();
+  deleteTextContent(tbody);
   for (let i = 0; i < dataArr.length; i+=1) {
     for (let j = 0; j < dataArr.length-1-i; j+=1) {
       if(dataArr[j][item] < dataArr[j+1][item]){
@@ -139,15 +113,83 @@ const sortingColumnDesc = (item) => {
   addRowsToTable(dataArr);
 }
 
+const editCell = (index) => {
+  deleteTextContent(rightDiv);
+
+  const form = createElement("form","form-group",null,rightDiv);
+
+  createElement("h3",null,"Change Cell Information",form);
+
+  for (let key in dataArr[index]) {
+    if(dataArr[index][key].length > 30 ){
+      createElement("textarea","edit-textarea",dataArr[index][key],form);
+    }else{
+      if(key === "id"){
+        continue;
+      }
+      let input = createElement("input","edit-input",null,form);
+      input.type = "text";
+      input.value = dataArr[index][key];
+    }
+  }
+  const btn = createElement("button","btn","Change",form);
+  btn.type = "submit"
+  btn.addEventListener("click",(e) => changeInfoToCell(e,index))
+
+  rightDiv.appendChild(form);
+}
+const changeInfoToCell = (e,index) => {
+ e.preventDefault();
+  let editObj = {
+    id:null,
+    firstName:null,
+    lastName:null,
+    about:null,
+    eyeColor:null,
+    phone:null
+  } ;
+  
+  for (let i = 0; i < e.srcElement.form.length; i++) {
+    switch (i) {
+      case 0:
+        editObj.firstName = e.srcElement.form[i].value;
+        break;
+      case 1:
+        editObj.lastName = e.srcElement.form[i].value;
+        break;
+      case 2:
+        editObj.about = e.srcElement.form[i].value;
+        break;
+      case 3:
+        editObj.eyeColor = e.srcElement.form[i].value;
+        break;
+      case 4:
+        editObj.phone = e.srcElement.form[i].value;
+        break;
+      default:
+        editObj.id = e.srcElement.form[i].value;
+        break;
+    }
+  }
+  if(editObj.hasOwnProperty("id")){
+    editObj.id = dataArr[index].id;
+  }
+  dataArr = [...dataArr.slice(0,index),editObj,...dataArr.slice(index+1)];
+
+  deleteTextContent(tbody);
+  deleteTextContent(rightDiv)
+  addRowsToTable(dataArr);
+}
+
+
+
+
 
 
 root = document.querySelector("#root"); // Получаем корень узел #root
 table = createTable(objHeader); // Создаем таблицу с заголовками
 tbody = createElement("tbody","t-body",null,table); // создаем tbody
-
-rightDiv = document.createElement("div");
-rightDiv.className = "rightDiv";
-root.appendChild(rightDiv);
+rightDiv = createElement("div","rightDiv",null,root)
 
 
 
